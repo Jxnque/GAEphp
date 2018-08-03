b006fc61e8a6e1ff745bdc5fc0a778791447356d
Jxnque <1602659741@qq.com>
2018-08-03 22:54:48 +0800

    1

--- GAEphp/write_in_bucket.php
+++ GAEphp/write_in_bucket.php
@@ -1,5 +1,11 @@
 <?php 
- 
+
+namespace Google\Cloud\Samples\Storage;
+
+# [START upload_object]
+
+use Google\Cloud\Storage\StorageClient;
+
 $handle = fopen('gs://jxn1-storage/prime_numbers.txt','w+') or die("Unable to open file!"); 
  
 fwrite($handle, "2"); for($i = 3; $i <= 2000; $i = $i + 2) {   $j = 2;   while($i % $j != 0) {     if($j > sqrt($i)) {       fwrite($handle, ", ".$i);       break;     }     $j++;   } } 
