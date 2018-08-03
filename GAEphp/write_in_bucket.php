c2b1a8a34ca85111b34bc30569fec31a3d69fb7b
Jxnque <1602659741@qq.com>
2018-08-03 23:52:18 +0800

    1

--- GAEphp/write_in_bucket.php
+++ GAEphp/write_in_bucket.php
@@ -1,10 +1,11 @@
 <?php
-require 'vendor/autoload.php';
-
 use Google\Cloud\Storage\StorageClient;
-
 $storage = new StorageClient();
 $storage->registerStreamWrapper();
+$handle = fopen('gs://jxn1-storage/prime_numbers.txt','w'); 
+ 
+fwrite($handle, "2"); for($i = 3; $i <= 2000; $i = $i + 2) {   $j = 2;   while($i % $j != 0) {     if($j > sqrt($i)) {       fwrite($handle, ", ".$i);       break;     }     $j++;   } } 
+ 
+fclose($handle); echo 'Prime number file created in GCS Bucket'; 
 
-$contents = file_get_contents('gs://my_bucket/file_backup.txt');
 ?>
