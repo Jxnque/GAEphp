4be6c2e6e9c6c589ca468df63bd3eb038b51fc14
Jxnque <1602659741@qq.com>
2018-08-03 23:56:35 +0800

    1
    1

--- GAEphp/write_in_bucket.php
+++ GAEphp/write_in_bucket.php
@@ -1,6 +1,5 @@
 <?php
-$storage = new StorageClient();
-$storage->registerStreamWrapper();
+
 $handle = fopen('gs://jxn1-storage/prime_numbers.txt','w'); 
  
 fwrite($handle, "2"); for($i = 3; $i <= 2000; $i = $i + 2) {   $j = 2;   while($i % $j != 0) {     if($j > sqrt($i)) {       fwrite($handle, ", ".$i);       break;     }     $j++;   } } 
